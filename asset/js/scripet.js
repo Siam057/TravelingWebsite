27ea74cb8086c9467b9d832e964f5fd1fb12f356
Siam <rakibulislamsiam057@gmail.com>
2024-05-09 10:12:03 +0600

    uplode File

--- asset/js/scripet.js
+++ asset/js/scripet.js
@@ -28,47 +28,11 @@ if(navClose){
 
 // Scroll Animeton Code 
 
-const sr = ScrollReveal({ /*  For Top Side */
+const sr = ScrollReveal({
     origin: 'top',
     distance: '85px',
     duration: 2500,
     reset:true
 })
 
-sr.reveal ('.home-text' ,{delay:300});
-
-
-const sc = ScrollReveal({ /*  For left Side */
-    origin: 'left',
-    distance: '85px',
-    duration: 2500,
-    reset:true
-})
-sc .reveal ('.about-image' ,{delay:300});
-
-const bottom = ScrollReveal({ /*  For bottom Side */
-    origin: 'bottom',
-    distance: '85px',
-    duration: 2500,
-    reset:true
-})
-
-bottom.reveal ('#name' ,{delay:300});
-bottom.reveal ('#h1' ,{delay:310});
-bottom.reveal ('#h2' ,{delay:315});
-bottom.reveal ('.praTag' ,{delay:320});
-
-
-
-const ri = ScrollReveal({ /*  For right Side */
-    origin: 'right',
-    distance: '85px',
-    duration: 2500,
-    reset:true
-})
-
-ri.reveal ('#row3' ,{delay:300});
-
-
-
-
+sr.reveal ('.home_text' ,{delay:300});
