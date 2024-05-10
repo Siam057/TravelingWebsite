d22e0cd1d2687627f952e4fd5089da0d6cf07c3b
Siam <rakibulislamsiam057@gmail.com>
2024-05-10 12:06:05 +0600

    uplode

--- asset/js/scripet.js
+++ asset/js/scripet.js
@@ -35,4 +35,44 @@ const sr = ScrollReveal({
     reset:true
 })
 
-sr.reveal ('.home_text' ,{delay:300});
+sr.reveal ('#name' ,{delay:300});
+sr.reveal ('#h1' ,{delay:300});
+sr.reveal ('#h2' ,{delay:300});
+sr.reveal ('.praTag' ,{delay:300});
+sr.reveal ('.serachBar' ,{delay:300});
+
+
+
+
+const ss = ScrollReveal({
+   origin: 'bottom',
+   distance: '85px',
+   duration: 2500,
+   reset:true
+})
+
+ss.reveal ('#name' ,{delay:300});
+ss.reveal ('.bottom' ,{delay:300});
+
+
+
+
+const sl = ScrollReveal({
+   origin: 'left',
+   distance: '85px',
+   duration: 2500,
+   reset:true
+})
+
+sl.reveal ('.left' ,{delay:300});
+
+
+
+const sp = ScrollReveal({
+   origin: 'right',
+   distance: '85px',
+   duration: 2500,
+   reset:true
+})
+
+sp.reveal ('.right' ,{delay:300});
